51691b421f2ac70f81ad852483ff69510d57c159
andrimuttaqien <andrineverstoptorun@gmail.com>
2021-08-16 13:47:20 +0700

    Update HomeController.php

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -18,99 +18,97 @@ use App\Models\Video;
 class HomeController extends Controller
 {
 
-    public function index()
-    {
-        $data = [
-            'title' => 'Home | Merdeka Belajar ITB'
-        ];
-        $data['info']                   = Post::where('id_category', '2')->where('status', 'Published')->get();
-        $data['berita']                 = Post::where('id_category', '1')->where('status', 'Published')->get();
-        $data['video']                  = Video::where('status', 'Enable')->get();
-        $data['slider']                 = Slider::where('status', 'Published')->get();
+	public function index()
+	{
+		$data = [
+			'title' => 'Home | Merdeka Belajar ITB'
+		];
+		$data['info']				= Post::where('id_category', '2')->where('status', 'Published')->orderByDesc('updated_at')->get();
+		$data['berita']				= Post::where('id_category', '1')->where('status', 'Published')->orderByDesc('updated_at')->get();
+		$data['video']				= Video::where('status', 'Enable')->orderByDesc('updated_at')->get();
+		$data['slider']				= Slider::where('status', 'Published')->orderByDesc('updated_at')->get();
 
-        $data['menu_kemendikbud']       = MenuOneChildAdmin::where('id_menu_parent', 2)->where('status', 'Enable')->get();
-        $data['menu_sub_kemendikbud']   = Page::where('id_menu_parent', 2)->where('id_menu_one_child', 2)->where('status', 'Published')->get();
+		$data['menu_kemendikbud']		= MenuOneChildAdmin::where('id_menu_parent', 2)->where('status', 'Enable')->get();
+		$data['menu_sub_kemendikbud']	= Page::where('id_menu_parent', 2)->where('id_menu_one_child', 2)->where('status', 'Published')->get();
 
-        $data['menu_itb']               = MenuOneChildAdmin::where('id_menu_parent', 3)->where('status', 'Enable')->get();
+		$data['menu_itb']			= MenuOneChildAdmin::where('id_menu_parent', 3)->where('status', 'Enable')->get();
 
-        $data['data']                   = Page::where('id_menu_parent', 1)->where('status', 'Published')->firstOrFail();
-        return view('pages.home', $data);
-    }
+		$data['data']				= Page::where('id_menu_parent', 1)->where('status', 'Published')->firstOrFail();
+		return view('pages.home', $data);
+	}
 
-    public function show($id, $url)
-    {
-        $url     = str_replace('-', ' ', strtoupper($url));
-        $data     = [
-            'title' => '' . $url . '' . ' | Merdeka Belajar ITB'
-        ];
-        $data['info']                   = Post::where('id_category', '2')->where('status', 'Published')->get();
-        $data['berita']                 = Post::where('id_category', '1')->where('status', 'Published')->get();
-        $data['video']                  = Video::where('status', 'Enable')->get();
-        $data['slider']                 = Slider::where('status', 'Published')->get();
+	public function show($id, $url)
+	{
+		$url 	= str_replace('-', ' ', strtoupper($url));
+		$data 	= [
+			'title' => '' . $url . '' . ' | Merdeka Belajar ITB'
+		];
+		$data['info']				= Post::where('id_category', '2')->where('status', 'Published')->orderByDesc('updated_at')->get();
+		$data['berita']				= Post::where('id_category', '1')->where('status', 'Published')->orderByDesc('updated_at')->get();
+		$data['video']				= Video::where('status', 'Enable')->orderByDesc('updated_at')->get();
+		$data['slider']				= Slider::where('status', 'Published')->orderByDesc('updated_at')->get();
 
-        $data['menu_kemendikbud']       = MenuOneChildAdmin::where('id_menu_parent', 2)->where('status', 'Enable')->get();
-        $data['menu_sub_kemendikbud']   = Page::where('id_menu_parent', 2)->where('id_menu_one_child', 2)->where('status', 'Published')->get();
+		$data['menu_kemendikbud']		= MenuOneChildAdmin::where('id_menu_parent', 2)->where('status', 'Enable')->get();
+		$data['menu_sub_kemendikbud']	= Page::where('id_menu_parent', 2)->where('id_menu_one_child', 2)->where('status', 'Published')->get();
 
-        $data['menu_itb']               = MenuOneChildAdmin::where('id_menu_parent', 3)->where('status', 'Enable')->get();
+		$data['menu_itb']			= MenuOneChildAdmin::where('id_menu_parent', 3)->where('status', 'Enable')->get();
+		$data['data']				= Page::where('id', $id)->where('status', 'Published')->firstOrFail();
+		return view('pages.show', $data);
+	}
 
-        $data['data']                   = Page::where('id', $id)->where('status', 'Published')->firstOrFail();
-        return view('pages.show', $data);
-    }
+	public function berita()
+	{
+		$data = [
+			'title' => 'Berita | Merdeka Belajar ITB'
+		];
+		$data['info']				= Post::where('id_category', '2')->where('status', 'Published')->orderByDesc('updated_at')->get();
+		$data['berita']				= Post::where('id_category', '1')->where('status', 'Published')->orderByDesc('updated_at')->get();
+		$data['video']				= Video::where('status', 'Enable')->orderByDesc('updated_at')->get();
+		$data['slider']				= Slider::where('status', 'Published')->orderByDesc('updated_at')->get();
 
-    public function berita()
-    {
-        $data = [
-            'title' => 'Berita | Merdeka Belajar ITB'
-        ];
-        $data['info']                   = Post::where('id_category', '2')->where('status', 'Published')->get();
-        $data['berita']                 = Post::where('id_category', '1')->where('status', 'Published')->get();
-        $data['video']                  = Video::where('status', 'Enable')->get();
-        $data['slider']                 = Slider::where('status', 'Published')->get();
+		$data['menu_kemendikbud']		= MenuOneChildAdmin::where('id_menu_parent', 2)->where('status', 'Enable')->get();
+		$data['menu_sub_kemendikbud']	= Page::where('id_menu_parent', 2)->where('id_menu_one_child', 2)->where('status', 'Published')->get();
 
-        $data['menu_kemendikbud']       = MenuOneChildAdmin::where('id_menu_parent', 2)->where('status', 'Enable')->get();
-        $data['menu_sub_kemendikbud']   = Page::where('id_menu_parent', 2)->where('id_menu_one_child', 2)->where('status', 'Published')->get();
+		$data['menu_itb']				= MenuOneChildAdmin::where('id_menu_parent', 3)->where('status', 'Enable')->get();
+		$data['menu_sub_itb']			= Page::where('id_menu_parent', 3)->where('id_menu_one_child', 3)->where('status', 'Published')->get();
+		
+		$data['data']				= Post::where('id_category', '1')->where('status', 'Published')->get();
+		return view('pages.berita', $data);
+	}
 
-        $data['menu_itb']               = MenuOneChildAdmin::where('id_menu_parent', 3)->where('status', 'Enable')->get();
+	public function info()
+	{
+		$data = [
+			'title' => 'Info | Merdeka Belajar ITB'
+		];
+		$data['info']				= Post::where('id_category', '2')->where('status', 'Published')->orderByDesc('updated_at')->get();
+		$data['berita']				= Post::where('id_category', '1')->where('status', 'Published')->orderByDesc('updated_at')->get();
+		$data['video']				= Video::where('status', 'Enable')->orderByDesc('updated_at')->get();
+		$data['slider']				= Slider::where('status', 'Published')->orderByDesc('updated_at')->get();
 
-        $data['data']                   = Post::where('id_category', '1')->where('status', 'Published')->get();
-        return view('pages.berita', $data);
-    }
+		$data['menu_kemendikbud']		= MenuOneChildAdmin::where('id_menu_parent', 2)->where('status', 'Enable')->get();
+		$data['menu_sub_kemendikbud']	= Page::where('id_menu_parent', 2)->where('id_menu_one_child', 2)->where('status', 'Published')->get();
+		
+		$data['menu_itb']			= MenuOneChildAdmin::where('id_menu_parent', 3)->where('status', 'Enable')->get();
+		$data['data']				= Post::where('id_category', '2')->where('status', 'Published')->get();
+		return view('pages.info', $data);
+	}
 
-    public function info()
-    {
-        $data = [
-            'title' => 'Info | Merdeka Belajar ITB'
-        ];
-        $data['info']                   = Post::where('id_category', '2')->where('status', 'Published')->get();
-        $data['berita']                 = Post::where('id_category', '1')->where('status', 'Published')->get();
-        $data['video']                  = Video::where('status', 'Enable')->get();
-        $data['slider']                 = Slider::where('status', 'Published')->get();
+	public function video()
+	{
+		$data = [
+			'title' => 'Video | Merdeka Belajar ITB'
+		];
+		$data['info']				= Post::where('id_category', '2')->where('status', 'Published')->orderByDesc('updated_at')->get();
+		$data['berita']				= Post::where('id_category', '1')->where('status', 'Published')->orderByDesc('updated_at')->get();
+		$data['video']				= Video::where('status', 'Enable')->orderByDesc('updated_at')->get();
+		$data['slider']				= Slider::where('status', 'Published')->orderByDesc('updated_at')->get();
 
-        $data['menu_kemendikbud']       = MenuOneChildAdmin::where('id_menu_parent', 2)->where('status', 'Enable')->get();
-        $data['menu_sub_kemendikbud']   = Page::where('id_menu_parent', 2)->where('id_menu_one_child', 2)->where('status', 'Published')->get();
-
-        $data['menu_itb']               = MenuOneChildAdmin::where('id_menu_parent', 3)->where('status', 'Enable')->get();
-
-        $data['data']                   = Post::where('id_category', '2')->where('status', 'Published')->get();
-        return view('pages.info', $data);
-    }
-
-    public function video()
-    {
-        $data = [
-            'title' => 'Video | Merdeka Belajar ITB'
-        ];
-        $data['info']                   = Post::where('id_category', '2')->where('status', 'Published')->get();
-        $data['berita']                 = Post::where('id_category', '1')->where('status', 'Published')->get();
-        $data['video']                  = Video::where('status', 'Enable')->get();
-        $data['slider']                 = Slider::where('status', 'Published')->get();
-
-        $data['menu_kemendikbud']       = MenuOneChildAdmin::where('id_menu_parent', 2)->where('status', 'Enable')->get();
-        $data['menu_sub_kemendikbud']   = Page::where('id_menu_parent', 2)->where('id_menu_one_child', 2)->where('status', 'Published')->get();
-
-        $data['menu_itb']               = MenuOneChildAdmin::where('id_menu_parent', 3)->where('status', 'Enable')->get();
-
-        $data['data']                   = Post::where('id_category', '2')->where('status', 'Published')->get();
-        return view('pages.video', $data);
-    }
+		$data['menu_kemendikbud']		= MenuOneChildAdmin::where('id_menu_parent', 2)->where('status', 'Enable')->get();
+		$data['menu_sub_kemendikbud']	= Page::where('id_menu_parent', 2)->where('id_menu_one_child', 2)->where('status', 'Published')->get();
+		
+		$data['menu_itb']			= MenuOneChildAdmin::where('id_menu_parent', 3)->where('status', 'Enable')->get();
+		$data['data']				= Post::where('id_category', '2')->where('status', 'Published')->get();
+		return view('pages.video', $data);
+	}
 }
